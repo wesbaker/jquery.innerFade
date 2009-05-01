b8f9dd6861c7339fd81643020301f43fc079290b
Wes Baker <wcbaker@gmail.com>
2009-04-30 22:10:37 -0400

    Simplifying plugin header.

--- jquery.innerfade.js
+++ jquery.innerfade.js
@@ -1,39 +1,10 @@
 /* =========================================================
-
 // jquery.innerfade.js
 
 // Date: 2009-03-04
 // Author: Wes Baker
 // Mail: wes@newcitymedia.com	
 // Web: http://www.newcitymedia.com
-
-// based on the work of Torsten Baldes http://medienfreunde.com, 
-// Matt Oakes http://portfolio.gizone.co.uk/applications/slideshow/
-// and Ralf S. Engelschall http://trainofthoughts.org/
-
- *
- *  <ul id="news"> 
- *      <li>content 1</li>
- *      <li>content 2</li>
- *      <li>content 3</li>
- *  </ul>
- *  
- *  $('#news').innerfade({ 
- *	  animationtype: Type of animation 'fade', 'slide' or 'slideOver' (Default: 'fade'), 
- *    easing: Type of easing for slideOver. jQuery comes with 'linear' and 'swing', but you can use other plugins (Default: 'linear'),
- *	  speed: Fading-/Sliding-Speed in milliseconds or keywords (slow, normal or fast) (Default: 'normal'), 
- *	  timeout: Time between the fades in milliseconds (Default: '2000'), 
- *	  type: Type of slideshow: 'sequence', 'random' or 'random_start' (Default: 'sequence'), 
- * 		containerheight: Height of the containing element in any css-height-value (Default: 'auto'),
- *	  runningclass: CSS-Class which the container get’s applied (Default: 'innerfade'),
- *	  children: optional children selector (Default: null)
- *  }); 
- *
-
-TODO Pull next and previous selectors into intitial declaration
-TODO Pause
-TODO Index
-
 // ========================================================= */
 
 
