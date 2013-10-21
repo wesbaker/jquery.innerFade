67e09d8dbe05e338d709d711d6c36451d735bd1f
Wes Baker <wes@wesbaker.com>
2013-10-21 12:56:25 -0400

    Adding missing semicolon.
    A few whitespace changes as well. Fixes #18.

--- jquery.innerfade.js
+++ jquery.innerfade.js
@@ -3,7 +3,7 @@
 
 // Date: 2010-07-23
 // Author: Wes Baker
-// Mail: wes@wesbaker.com	
+// Mail: wes@wesbaker.com
 // Web: http://www.wesbaker.com
 // ========================================================= */
 
@@ -21,7 +21,7 @@
 		'containerHeight':			'auto',
 		'runningClass':				'innerFade',
 		'children':					null,
-		'cancelLink':				null, 
+		'cancelLink':				null,
 		'pauseLink':				null,
 		'prevLink':					null,
 		'nextLink':					null,
@@ -30,13 +30,13 @@
 		'totalItemsContainer': 		null,
 		'callback_index_update': 	null
 	};
-	
+
 	$(function() {
 	    window.isActive = true;
 	    $(window).focus(function() { this.isActive = true; });
 	    $(window).blur(function() { this.isActive = false; });
-	});	
-	
+	});
+
 	$.fn.innerFade = function(options) {
 		return this.each(function() {
 			$fade_object = new Object();
@@ -44,7 +44,7 @@
 			$fade_object.container = this;
 			// Combine default and set settings or use default
 			// Pay attention kids, there's an important lesson here. When using $.extend, the first parameter will
-			// be CHANGED to the combination of all the parameters. In my situation, I just wanted to combine two 
+			// be CHANGED to the combination of all the parameters. In my situation, I just wanted to combine two
 			// objects, but not affect them in any way hence the empty object.
 			$fade_object.settings = $.extend({}, default_options, options);
 			// If children option is set use that as elements, otherwise use the called jQuery object
@@ -53,12 +53,12 @@
 			$fade_object.count = 0;
 			// Save data to container for use later
 			$($fade_object.container).data('object', $fade_object);
-			
+
 			// Start the loop
 			if ($fade_object.elements.length > 1) {
 				// Establish the Next and Previous Handlers
-				if ($fade_object.settings.nextLink || $fade_object.settings.prevLink) { 
-					$.bindControls($fade_object); 
+				if ($fade_object.settings.nextLink || $fade_object.settings.prevLink) {
+					$.bindControls($fade_object);
 				}
 
 				// Establish Cancel Handler
@@ -74,7 +74,7 @@
 				};
 
 				// Build the Index if one is specified
-				if ($fade_object.settings.indexContainer) {				
+				if ($fade_object.settings.indexContainer) {
 					$.innerFadeIndex($fade_object);
 				};
 
@@ -89,9 +89,9 @@
 
 				if ($fade_object.settings.type == "random") {
 					toHide = Math.floor(Math.random() * $fade_object.elements.length);
-					do { 
+					do {
 						toShow = Math.floor(Math.random() * $fade_object.elements.length);
-					} while (toHide == toShow );				
+					} while (toHide == toShow );
 					$($fade_object.elements[toHide]).show();
 				} else if ( $fade_object.settings.type == 'random_start' ) {
 					$fade_object.settings.type = 'sequence';
@@ -129,7 +129,7 @@
 			}
 		});
 	};
-	
+
 	/**
 	 * Public function to change to a specific slide. This is expecting a zero-index slide number.
 	 * @param {Number} slide_number Zero-indexed slide number
@@ -137,7 +137,7 @@
 	$.fn.innerFadeTo = function(slide_number) {
 		return this.each(function(index) {
 			var $fade_object = $(this).data('object');
-			
+
 			var $currentVisibleItem = $($fade_object.elements).filter(':visible');
 			var currentItemIndex = $($fade_object.elements).index($currentVisibleItem);
 			$.stopSlideshow($fade_object);
@@ -153,9 +153,9 @@
 	 * @param {Number} toShow The position in the elements array of the item to be shown
 	 * @param {Number} toHide The position in the elements array of the item to be hidden
 	 */
-	$.fadeToItem = function($fade_object, toShow, toHide) {		
+	$.fadeToItem = function($fade_object, toShow, toHide) {
 		var speed = $fade_object.settings.speed;
-		
+
 		switch ($fade_object.settings.animationType) {
 			case "slide":
 				$($fade_object.elements[toHide]).slideUp(speed);
@@ -167,50 +167,50 @@
 					to_show_css = {},
 					to_hide_animation = {},
 					to_show_animation = {};
-					
+
 				$($fade_object.container).css({'overflow': 'hidden'});
-				
+
 				// Both CSS Declarations use the same initial CSS
 				to_hide_css = {
 					'position': 'absolute',
 					'top': '0px'
 				};
-				
+
 				to_show_css = $.extend({}, to_hide_css);
-				
+
 				// If going forward, we want the item (to be shown) to animate from the right to left
 				// If going backwards, we want the item (to be shown) to animate from the left to the right
 				if (toShow > toHide) { // Forwards
 					to_hide_css.left = "0px";
 					to_hide_css.right = "auto";
-	
+
 					to_show_css.left = 'auto';
 					to_show_css.right = '-' + itemWidth + 'px';
-	
+
 					to_hide_animation.left = '-' + itemWidth + 'px';
-	
+
 					to_show_animation.right = '0px';
-					
+
 					console.log(to_hide_css);
 				} else { // Backwards
 					to_hide_css.left = "auto";
 					to_hide_css.right = "0px";
-	
+
 					to_show_css.left = '-' + itemWidth + 'px';
 					to_show_css.right = 'auto';
-	
+
 					to_hide_animation.right = '-' + itemWidth + 'px';
-	
+
 					to_show_animation.left = '0px';
 				};
-				
+
 				$($fade_object.elements[toHide]).css(to_hide_css);
 				$($fade_object.elements[toShow]).css(to_show_css).show();
-	
+
 				$($fade_object.elements[toHide]).animate(to_hide_animation, speed, $fade_object.settings.easing, function() {
 					$(this).hide();
 				});
-				
+
 				$($fade_object.elements[toShow]).animate(to_show_animation ,speed, $fade_object.settings.easing);
 				break;
 			case "fadeEmpty":
@@ -224,16 +224,16 @@
 				});
 				break;
 			default:
-				$($fade_object.elements[toHide]).fadeOut(speed)
+				$($fade_object.elements[toHide]).fadeOut(speed);
 				$($fade_object.elements[toShow]).fadeIn(speed);
 			break;
 		}
-		
+
 		// Update the toShow item
 		if ($fade_object.settings.currentItemContainer) {
 			$.currentItem($fade_object, toShow);
 		};
-		
+
 		// Update indexes with active classes
 		if ($fade_object.settings.indexContainer || $fade_object.settings.callback_index_update) {
 			$.updateIndexes($fade_object, toShow);
@@ -248,17 +248,17 @@
 	 * @param {Boolean} firstRun If this is the first run of innerfade, pass true, otherwise pass false
 	 */
 	$.fadeTimeout = function($fade_object, toShow, toHide, firstRun) {
-		
+
 		// only process if window is active, otherwise just call the same function
 		if (window.isActive) {
 			// If its not the first run, then fade
 			if (firstRun != true) {
 				$.fadeToItem($fade_object, toShow, toHide);
 			};
-		
+
 			// Increment the count of slides shown
 			$fade_object.count++;
-		
+
 			// 	Check if loop is false, if it is check to see how many slides have been shown.
 			// In the case that you're at the last slide, stop the slideshow and return.
 			if ($fade_object.settings.loop == false && $fade_object.count >= $fade_object.elements.length) {
@@ -274,9 +274,9 @@
 				toHide = (toHide > toShow) ? 0 : toShow;
 				toShow = (toShow + 1 >= $fade_object.elements.length) ? 0 : toShow + 1;
 			}
-			
+
 		};
-		
+
 		// Set the time out; if its first run and a start delay exists, use the start delay
 		var timeout = (firstRun && $fade_object.settings.startDelay) ? $fade_object.settings.startDelay : $fade_object.settings.timeout;
 		$($fade_object.container).data('current_timeout', setTimeout((function() { $.fadeTimeout($fade_object, toShow, toHide, false); }), timeout));
@@ -289,7 +289,7 @@
 			$.stopSlideshow($fade_object);
 		});
 	};
-	
+
 	/**
 	 * Stops the slideshow
 	 * @param {Object} $fade_object The object that contains the settings, elements and container for this slideshow
@@ -298,7 +298,7 @@
 		clearTimeout($($fade_object.container).data('current_timeout'));
 		$($fade_object.container).data('current_timeout', null);
 	};
-	
+
 	/**
 	 * Establishes the Next and Previous link behavior
 	 * @param {Object} $fade_object The object that contains the settings, elements and container for this slideshow
@@ -307,48 +307,48 @@
 		$($fade_object.settings.nextLink).on('click', function(event) {
 			event.preventDefault();
 			$.stopSlideshow($fade_object);
-			
+
 			var $currentElement = $($fade_object.elements).filter(':visible');
 			var currentElementIndex = $($fade_object.elements).index($currentElement);
 
 			var $nextElement = ($currentElement.next().length > 0) ? $currentElement.next() : $($fade_object.elements).filter(':first');
 			var nextElementIndex = $($fade_object.elements).index($nextElement);
-			
+
 			$.fadeToItem($fade_object, nextElementIndex, currentElementIndex);
 		});
-			
+
 		$($fade_object.settings.prevLink).on('click', function(event) {
 			event.preventDefault();
 			$.stopSlideshow($fade_object);
-			
+
 			var $currentElement = $($fade_object.elements).filter(':visible');
 			var currentElementIndex = $($fade_object.elements).index($currentElement);
 
 			var $previousElement = ($currentElement.prev().length > 0) ? $currentElement.prev() : $($fade_object.elements).filter(':last');
 			var previousElementIndex = $($fade_object.elements).index($previousElement);
-			
+
 			$.fadeToItem($fade_object, previousElementIndex, currentElementIndex);
 		});
 	};
-	
+
 	/**
 	 * Establishes the Pause Button
 	 * @param {Object} $fade_object The object that contains the settings, elements and container for this slideshow
 	 */
-	$.bind_pause = function($fade_object) {		
+	$.bind_pause = function($fade_object) {
 		$($fade_object.settings.pauseLink).unbind().click(function(event) {
-			event.preventDefault(); 
+			event.preventDefault();
 			if ($($fade_object.container).data('current_timeout') != null) {
 				$.stopSlideshow($fade_object);
 			} else {
-				// Restart the slideshow				
+				// Restart the slideshow
 				var tag = $($fade_object.container).children(':first').attr('tagName').toLowerCase();
 				var nextItem = '';
 				var previousItem = '';
-				
+
 				if ($fade_object.settings.type == "random") {
 					previousItem = Math.floor(Math.random() * $fade_object.elements.length);
-					do { 
+					do {
 						nextItem = Math.floor(Math.random() * $fade_object.elements.length);
 					} while (previousItem == nextItem);
 				} else if ($fade_object.settings.type == "random_start") {
@@ -358,12 +358,12 @@
 					previousItem = $(tag, $($fade_object.container)).index($(tag+':visible', $($fade_object.container)));
 					nextItem = ((previousItem + 1) == $fade_object.elements.length) ? 0 : previousItem + 1;
 				}
-				
+
 				$.fadeTimeout($fade_object, nextItem, previousItem, false);
 			}
 		});
 	};
-		
+
 	/**
 	 * Establishes the Cancel Button
 	 * @param {Object} $fade_object The object that contains the settings, elements and container for this slideshow
@@ -383,13 +383,13 @@
 	$.updateIndexes = function($fade_object, toShow) {
 		$($fade_object.settings.indexContainer).children().removeClass('active');
 		$('> :eq(' + toShow + ')', $($fade_object.settings.indexContainer)).addClass('active');
-		
+
 		// Check for the callback index update
 		if (typeof($fade_object.settings.callback_index_update) == "function") {
 			$fade_object.settings.callback_index_update.call(this, toShow);
 		};
 	};
-	
+
 	/**
 	 * Creates handlers for the links created by the $.handleIndexes and $.generateIndexes functions
 	 * @param {Object} $fade_object The object that contains the settings, elements and container for this slideshow
@@ -407,21 +407,21 @@
 			};
 		});
 	};
-	
+
 	/**
 	 * Creates one link for each item in the slideshow, to show that item immediately
 	 * @param {Object} $fade_object The object that contains the settings, elements and container for this slideshow
 	 */
 	$.createIndexes = function($fade_object) {
 		var $indexContainer = $($fade_object.settings.indexContainer);
-		
+
 		for (var i=0; i < $fade_object.elements.length; i++) {
 			var	$link = $('<li><a href="#">' + (i + 1) + '</a></li>');
 			$.createIndexHandler($fade_object, i, $link);
 			$indexContainer.append($link);
 		};
 	};
-	
+
 	/**
 	 * Establishes links between the slide elements and index items in the indexContainer
 	 * @param {Object} $fade_object The object that contains the settings, elements and container for this slideshow
@@ -429,7 +429,7 @@
 	$.linkIndexes = function($fade_object) {
 		var $indexContainer = $($fade_object.settings.indexContainer);
 		var $indexContainerChildren = $('> :visible', $indexContainer);
-		
+
 		if ($indexContainerChildren.size() == $fade_object.elements.length) {
 			var count = $fade_object.elements.length;
 			for (var i=0; i < count; i++) {
@@ -438,11 +438,11 @@
 			};
 		} else {
 			alert("There is a different number of items in the menu and slides. There needs to be the same number in both.\nThere are " + $indexContainerChildren.size() + " in the indexContainer.\nThere are " + $fade_object.elements.length + " in the slides container.");
-		};		
+		};
 	};
-	
+
 	/**
-	 * Determines if the index container is empty or not. If its empty then it generates links, if its not empty 
+	 * Determines if the index container is empty or not. If its empty then it generates links, if its not empty
 	 * it links one to one
 	 * @param {Object} $fade_object The object that contains the settings, elements and container for this slideshow
 	 */
@@ -454,7 +454,7 @@
 			$.linkIndexes($fade_object);
 		};
 	};
-	
+
 	/**
 	 * Changes the text of the current item selector to the index of the current item
 	 * @param {Object} $fade_object The object that contains the settings, elements and container for this slideshow
@@ -464,7 +464,7 @@
 		var $container = $($fade_object.settings.currentItemContainer);
 		$container.text(current + 1);
 	};
-	
+
 	/**
 	 * Changes the text of the total item selector to the total number of items
 	 * @param {Object} $fade_object The object that contains the settings, elements and container for this slideshow
